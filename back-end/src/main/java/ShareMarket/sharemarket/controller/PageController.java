5b6a6864d18470f17ccaf05825e8668677f88605
SeungJin Lee <seungjin9777@gmail.com>
2021-04-15 16:11:29 +0900

    게시글 페이징기능 구현완료

--- back-end/src/main/java/ShareMarket/sharemarket/controller/PageController.java
+++ back-end/src/main/java/ShareMarket/sharemarket/controller/PageController.java
@@ -20,25 +20,9 @@ public class PageController {
 
     private final PagingService pagingService;
 
-    @Autowired
-    private final PostsRepository postsRepository;
-
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping("/api/posts/page")
     public Page<PagingDto> paging(@PageableDefault(size=5, sort = "createdDate") Pageable pageRequest) {
         return pagingService.paging(pageRequest);
     }
-//    public Page<PagingDto> paging(@PageableDefault(size=5, sort = "createdTime") Pageable pageRequest){
-//
-//        Page<Post> postList = postsRepository.findAll(pageRequest);
-//
-//        Page<PagingDto> pagingList = postList.map(
-//                post -> new PagingDto(
-//                        post.getId(),
-//                        post.getTitle(),
-//                        post.getUser_id(),
-//                        post.getCreatedDate()
-//                ));
-//        return pagingList;
-//    }
 }
