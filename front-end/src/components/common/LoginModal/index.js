ddd3853d44f6a2d33dfb94a65415d503a26fe2cd
HaYeong <hy78733@gmail.com>
2021-04-19 22:01:23 +0900

    LoginModal mockup

---
+++ front-end/src/components/common/LoginModal/index.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import { Avatar, Button, TextField, Typography } from '@material-ui/core';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+
+const modalStyle = {
+  top: `30%`,
+  left: `50%`,
+  transform: `translate(-50%, -30%)`,
+}
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    position: 'fixed',
+    width: 400,
+    height: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+  formPaper: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 0),
+  },
+}));
+
+export default function LoginModal() {
+  const classes = useStyles();
+  // getModalStyle is not a pure function, we roll the style only on the first render
+
+  return (
+    <Modal style={modalStyle} className={classes.modal}
+      open={true}
+      onClose={() => { }}
+    >
+      <div className={classes.formPaper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          로그인
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="아이디"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="비밀번호"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            로그인
+          </Button>
+        </form>
+      </div>
+    </Modal>
+  );
+}
