e386482c687d2f695bafd41f5a98e142008f0059
HaYeong <hy78733@gmail.com>
2021-04-30 23:03:09 +0900

    add HeaderMenu, connect to Header
    logged가 true여도 버튼 안바뀜.
    새로고침하면 logged 초기화됨

--- front-end/src/components/common/Header/index.js
+++ front-end/src/components/common/Header/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -6,6 +6,8 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import { useDispatch, useSelector } from 'react-redux';
 import { toggleModal } from 'store/modules/store';
+import HeaderMenu from './HeaderMenu';
+import { Link as RouterLink } from 'react-router-dom';
 
 const useStyles = makeStyles(theme => ({
 	root: {
@@ -23,6 +25,14 @@ export default function Header() {
 	const onClickLogin = () => {
 		dispatch(toggleModal('loginModal')); //로그인 모달 열기
 	};
+	const [anchorEl, setAnchorEl] = useState(null);
+	const handleClick = event => {
+		setAnchorEl(event.currentTarget);
+	};
+
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
 
 	return (
 		<div className={classes.root}>
@@ -31,23 +41,31 @@ export default function Header() {
 					<Typography variant="h6" className={classes.title}>
 						로고
 					</Typography>
+					{/* !logged로 바꿔야 됨! */}
 					{!logged ? (
 						<div>
 							<Button color="inherit" onClick={onClickLogin}>
 								로그인
 							</Button>
-							<Button variant="outlined" color="inherit">
+							<Button variant="outlined" component={RouterLink} to='/join' color="inherit">
 								회원가입
 							</Button>
 						</div>
 					) : (
 						<div>
-							<Button color="inherit" onClick={onClickLogin}>
-								로그아웃
+							<Button color="inherit" component={RouterLink} to='/post/editor'>
+								글 올리기
 							</Button>
-							<Button variant="outlined" color="inherit">
-								마이페이지
+							<Button
+								aria-controls="header-menu"
+								aria-haspopup="true"
+								variant="outlined"
+								color="inherit"
+								onClick={handleClick}
+							>
+								메뉴
 							</Button>
+							<HeaderMenu anchorEl={anchorEl} onClose={handleClose} />
 						</div>
 					)}
 				</Toolbar>
