bc39423e368ba72928ee5273397c84d81b571960
jiyoungtt <jiyoungtt@naver.com>
2021-05-02 20:04:39 +0900

    state tab navigator for mypage

--- front-end/src/components/App.js
+++ front-end/src/components/App.js
@@ -36,6 +36,8 @@ const App = () => {
             <Route exact path='/post/:post_id' component={NewPostPage} />
 
             <Route exact path='/list/:pageNumber?' component={ListPage} />
+            <Route exact path='/mypage/borrower' component={BorrowerPage} />
+            <Route exact path='/mypage/borrowee' component={BorroweePage} />
 
             <Route component={NotFoundPage} />
           </Switch>
