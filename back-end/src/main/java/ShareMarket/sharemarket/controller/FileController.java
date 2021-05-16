860c03319a2f4c0dc46f77c56dc029ac2150aca1
SeungJin Lee <seungjin9777@gmail.com>
2021-05-17 02:40:16 +0900

    pathVariable규칙 통일

--- back-end/src/main/java/ShareMarket/sharemarket/controller/FileController.java
+++ back-end/src/main/java/ShareMarket/sharemarket/controller/FileController.java
@@ -25,26 +25,26 @@ public class FileController {
 
     // 파일업로드
     // 요청에서 파일객체를 받아 FileDto리스트에 담아서 DB에 저장하고 결과를 반환함
-    @PostMapping("/api/file/{id}")
-    public List<FileResponseDto> uploadMultipleFiles(@RequestParam("files") MultipartFile[] files, @PathVariable Long id) {
-        List<FileResponseDto> fileDtoList = fileService.uploadFiles(files, id);
+    @PostMapping("/api/file/{postId}")
+    public List<FileResponseDto> uploadMultipleFiles(@RequestParam("files") MultipartFile[] files, @PathVariable Long postId) {
+        List<FileResponseDto> fileDtoList = fileService.uploadFiles(files, postId);
         return fileDtoList;
     }
 
     // 게시글PK에 연관된 전체 파일조회
     // FileDto로 반환한다. -> 밑에거에서 id값은 빠짐
-    @GetMapping("/api/file/{id}")
-    public List<FileResponseDto> getFiles(@PathVariable Long id) {
-        return fileService.getFile(id);
+    @GetMapping("/api/file/{postId}")
+    public List<FileResponseDto> getFiles(@PathVariable Long postId) {
+        return fileService.getFile(postId);
     }
 
-    @DeleteMapping("/api/file/{fileId}")
-    public ResponseEntity<?> deleteFile(@PathVariable Long fileId) throws FileNotFoundException {
-        fileService.deleteFile(fileId);
+    @DeleteMapping("/api/file/{id}")
+    public ResponseEntity<?> deleteFile(@PathVariable Long id) throws FileNotFoundException {
+        fileService.deleteFile(id);
         return new ResponseEntity(DefaultRes.response(
                 HttpStatusCode.OK,
                 HttpResponseMessage.DELETE_SUCCESS,
-                fileId), HttpStatus.OK
+                id), HttpStatus.OK
         );
     }
 
