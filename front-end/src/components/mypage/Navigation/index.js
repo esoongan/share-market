e40ac465ae285e7541e4dada6652eb0c699c3d68
jiyoungtt <jiyoungtt@naver.com>
2021-05-19 15:38:58 +0900

    modify navigation button

---
+++ front-end/src/components/mypage/Navigation/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import { makeStyles } from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    paddingTop: theme.spacing(5),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+}));
+
+
+export default function Navigation() {
+    const classes = useStyles();
+  
+    return (
+      <div className={classes.root}>
+        <ButtonGroup sizs='large' variant="text" color="primary" aria-label="text primary button group">
+          <Button> BUYER</Button>
+          <Button>SELLER</Button>
+        </ButtonGroup>
+      </div>
+    );
+  };
+
+
+
--- front-end/src/pages/BorrowerPage.js
+++ front-end/src/pages/BorrowerPage.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Renting from '../components/mypage/Renting';
 import Mypost from '../components/mypage/Mypost';
 import Reservation from '../components/mypage/Reservation';
+import Navigation from 'components/mypage/Navigation';
 
 
 const BorrowerPage = () => {
   return (
     <>
+    <Navigation/>
     <Renting/>
     <Reservation/>
     <Mypost/>
