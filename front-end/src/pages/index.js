0010e11ebcd84f9e68f102fb08bbdf4586d9f2f5
HaYeong <hy78733@gmail.com>
2021-05-30 14:08:43 +0900

    Join button error fixed

--- front-end/src/pages/index.js
+++ front-end/src/pages/index.js
@@ -3,7 +3,6 @@ export { default as JoinPage } from './JoinPage'
 export { default as EditorPage} from './EditorPage'
 export { default as NotFoundPage } from './NotFoundPage'
 export { default as PostPage } from './PostPage'
-export { default as NewPostPage } from './PostPage'
 
 export { default as ListPage } from './ListPage'
 export { default as BorrowerPage } from './BorrowerPage'
