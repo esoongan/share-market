a2ba4301939c3e19f3b95beeb077ef04ef9e6488
HaYeong <hy78733@gmail.com>
2021-06-11 20:54:20 +0900

    Contracts - add Switch Button, contract's state

--- front-end/src/components/mypage/Contracts/index.js
+++ front-end/src/components/mypage/Contracts/index.js
@@ -6,7 +6,7 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Typography from '@material-ui/core/Typography';
-import { Button, Divider, Hidden, IconButton, Radio } from '@material-ui/core';
+import { Button, Divider, FormControlLabel, Hidden, IconButton, Radio, Switch } from '@material-ui/core';
 import { grey } from '@material-ui/core/colors';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
@@ -16,11 +16,18 @@ import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
 import moment from 'moment';
 import SendIcon from '@material-ui/icons/Send';
 import ChatModal from 'components/common/ChatModal';
+import {contractState} from 'constant/constant';
 
 const useStyles = makeStyles(theme => ({
 	root: {
 		marginBottom: theme.spacing(10),
 		height: 350,
+	},
+	header: {
+		display: 'flex',
+		'& h1': {
+			marginRight: theme.spacing(6),
+		}
 	},
 	container: {
 		display: 'flex',
@@ -63,11 +70,34 @@ const useStyles = makeStyles(theme => ({
 	},
 }));
 
-const getPrice = ({ start, end, price }) => {
+const getDays = ({ start, end }) => {
 	const startDate = moment(start, 'YYYY-MM-DD');
 	const endDate = moment(end, 'YYYY-MM-DD');
-	return price * (endDate.diff(startDate, 'days') + 1);
+	return (endDate.diff(startDate, 'days') + 1);
 };
+const getState = ({state, start, end}) => {
+	const startDate = moment(start, 'YYYY-MM-DD');
+	const endDate = moment(end, 'YYYY-MM-DD');
+
+	if(state === 'default'){
+		if(startDate.isBefore(moment())){
+			return contractState['expired'];	//수락하지 않고 요청 시작 날짜가 지났을 때
+		}
+		return contractState['waiting'];
+	}
+	else if(state === 'accept'){
+		if(endDate.isBefore(moment())){
+			return contractState['completed'];	//수락되었고 대여 종료 날짜가 지났을 때
+		}
+		else if(startDate.isBefore(moment())){
+			return contractState['ing'];	//수락되었고 대여 중일 때
+		}
+		return contractState['reserved'];
+	}
+	else if(state === 'refused'){
+		return contractState['refused'];
+	}
+}
 
 // 포스트 별 들어온 거래 요청을 테이블로 보여주는 컴포넌트
 function ContractTable({
@@ -81,7 +111,6 @@ function ContractTable({
 	onClickChat,
 }) {
 	const classes = useStyles();
-	console.log(row);
 	//TODO: 포스트 정보 가져오기 (렌탈료), 보낸 사람 pk 말고 username으로 넣기
 	return (
 		<div style={{ flexGrow: 1 }}>
@@ -90,7 +119,7 @@ function ContractTable({
 					<NavigateBeforeIcon />
 				</IconButton>
 				<Typography variant="h6" gutterBottom component="div">
-					{row[0].postId} 요청 내역
+					{row[0].postId}
 				</Typography>
 				<IconButton aria-label="next post" onClick={onClickNext}>
 					<NavigateNextIcon />
@@ -108,32 +137,34 @@ function ContractTable({
 						<TableCell>상태</TableCell>
 						<TableCell>대여 날짜</TableCell>
 						<TableCell>반납 날짜</TableCell>
-						<TableCell align="right">총 렌탈료(₩)</TableCell>
+						{/* <TableCell align="right">총 일수</TableCell>  */}
 						<TableCell align="right">보낸 사람</TableCell>
-						<TableCell>대화하기</TableCell>
+						<TableCell>채팅</TableCell>
 					</TableRow>
 				</TableHead>
 				<TableBody>
-					{row.map(contract => (
+					{row.map(contract => {
+						const state = getState({state: contract.state, start:contract.startDate, end:contract.endDate})
+						return (
 						<TableRow className={classes.tableRow} key={contract.id}>
 							<TableCell>
 								<Radio
+									disabled= {state !== contractState['waiting']}
 									checked={selectedContract === contract.id.toString()}
 									onChange={onChangeRadio}
 									value={contract.id}
 									name="radio-button-contract"
 								/>
 							</TableCell>
-							<TableCell>{contract.state}</TableCell>
+							<TableCell>{state}</TableCell>
 							<TableCell>{contract.startDate}</TableCell>
 							<TableCell>{contract.endDate}</TableCell>
-							<TableCell align="right">
-								{getPrice({
+							{/* <TableCell align="right">
+								{getDays({
 									start: contract.startDate,
 									end: contract.endDate,
-									price: 1000,
 								})}
-							</TableCell>
+							</TableCell> */}
 							<TableCell align="right">{contract.buyerId}</TableCell>
 							<TableCell>
 								<IconButton
@@ -149,7 +180,7 @@ function ContractTable({
 								</IconButton>
 							</TableCell>
 						</TableRow>
-					))}
+					)})}
 				</TableBody>
 			</Table>
 			<div className={selectedContract !== -1 ? classes.buttons : classes.hide}>
@@ -180,6 +211,7 @@ export default function Contracts({
 	onClickAccept,
 	onClickRefuse,
 	openChatModal,
+	mode,
 }) {
 	const classes = useStyles();
 	const [selectedIndex, setSelectedIndex] = React.useState(0);
@@ -217,9 +249,17 @@ export default function Contracts({
 	};
 	return (
 		<div className={classes.root}>
+			<div className={classes.header}>
+
 			<Typography gutterBottom variant="h5" component="h1">
 				거래 요청 내역
 			</Typography>
+			<FormControlLabel
+        control={<Switch checked={mode} name="mode" />}
+        label="전체 내역"
+      />
+			</div>
+
 			<div className={classes.container}>
 				<Hidden smDown>
 					<List
@@ -227,7 +267,8 @@ export default function Contracts({
 						component="nav"
 						aria-label="post list"
 					>
-						{postList.map((post, index) => (
+						{postList.length>0 ? 
+						postList.map((post, index) => (
 							<ListItem
 								key={index}
 								button
@@ -236,7 +277,11 @@ export default function Contracts({
 							>
 								<ListItemText primary={post} />
 							</ListItem>
-						))}
+						)) : 
+						<ListItem>
+							<ListItemText primary='비어있음' />
+						</ListItem>
+						}
 					</List>
 				</Hidden>
 				<Divider className={classes.divider} orientation="vertical" />
---
+++ front-end/src/constant/constant.js
@@ -0,0 +1,8 @@
+export const contractState = {  //거래 상태
+  waiting : '대기중',
+  reserved: '예약중',
+  completed: '완료',
+  refused: '거절됨',
+  ing: '진행중',
+  expired: '만료됨',
+}
